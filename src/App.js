bbfbd37b608fb8d719bb28480cfba9b63dbd5b5e
Charlie Taminian <ctaminian@gmail.com>
2019-04-10 22:41:02 -0700

    feat: Add title and description section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <main>
+        <section>
+          <h1>WEATHER<br/>ME<span>NOW</span></h1>
+          <hr />
+          <p>A minimal weather app design to brighten up your day.<br />Designed and developed by Charlie Taminian @ Bone Crush Media.</p>
+        </section>
+      </main>
     );
   }
 }
