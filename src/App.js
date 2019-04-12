0b500becfc21637c7631603fc6e635192686ab5d
Charlie Taminian <ctaminian@gmail.com>
2019-04-11 22:12:33 -0700

    feat: Clean up app into components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Info from './Components/Info/Info';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <main>
-        <section>
-          <h1>WEATHER<br/>ME<span>NOW</span></h1>
-          <hr />
-          <p>A minimal weather app design to brighten up your day.<br />Designed and developed by Charlie Taminian @ Bone Crush Media.</p>
-          <div className='bottom'>
-            <p>Your weather is currently showing in:</p>
-            <div className='buttons'>
-              <div className='btn'>
-                <button>C</button>
-                <p>Celsius</p>
-              </div>
-              <div className='btn'>
-                <button className='clicked'>F</button>
-                <p>Fahrenheit</p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-    );
-  }
+const App = () => {
+  return (
+    <main>
+      <Info />
+    </main>
+  );
 }
 
 export default App;
