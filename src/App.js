9858f6fee0e4fc7dfad2dd87c2a7fcba40912f22
Charlie Taminian <ctaminian@gmail.com>
2019-04-11 21:44:57 -0700

    feat: Finish main section

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ class App extends Component {
           <h1>WEATHER<br/>ME<span>NOW</span></h1>
           <hr />
           <p>A minimal weather app design to brighten up your day.<br />Designed and developed by Charlie Taminian @ Bone Crush Media.</p>
+          <div className='bottom'>
+            <p>Your weather is currently showing in:</p>
+            <div className='buttons'>
+              <div className='btn'>
+                <button>C</button>
+                <p>Celsius</p>
+              </div>
+              <div className='btn'>
+                <button className='clicked'>F</button>
+                <p>Fahrenheit</p>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
     );
